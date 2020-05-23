52f4b3c818083308a3fb1cb454c374607b7d849b
karla98 <riosdiazkm@gmail.com>
2020-05-22 19:30:01 -0500

    proyecto final

---
+++ keys.js
@@ -0,0 +1,28 @@
+/*
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'videos_favoritos'
+
+    }
+
+};
+*/
+
+
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'remotemysql.com',
+        user: 'T0jyTP8cuJ',
+        password: 'oKvvw3OuJZ',
+        database: 'T0jyTP8cuJ'
+
+    }
+
+};
